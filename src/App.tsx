d30fceee0851379af81071fd035a17e7cfe4620a
Rushikesh Argade <code.rushi07@gmail.com>
2025-05-20 12:55:08 +0530

    Added initial path to redirect to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard/Dashboard'
 import ProductList from './pages/ProductList/ProductList'
@@ -10,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path='/home' element={<Dashboard />} />
           <Route path='/product-list' element={<ProductList />} />
         </Routes>
